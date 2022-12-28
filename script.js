8d44d993d8eea560010f169b3fa763cc5365c2e6
Shakh Khaytimatov <Khaytimat@gmail.com>
2022-12-28 14:10:05 +0300

    second commit

--- script.js
+++ script.js
@@ -1,183 +1,309 @@
-// const output = document.querySelector('#output')
+renderTodos()
 
+let taskInputValue = document.getElementById('userInput')
+let addTaskBtn = document.getElementById('push')
+let saveTaskBtn = document.getElementById('save')
+let deleteAll = document.getElementById('deleteAll')
+let searchInput = document.getElementById('searchBar')
 
-// const date = new Date()
-// console.log('день: ',date.getDate());
-// console.log(date.getMonth()+1)
-// console.log(date.getFullYear());
-// console.log(date.getHours());
-// console.log(date.getMinutes());
-// console.log(date.getSeconds());
-// console.log(date.toISOString());
-
-// const currentDate = `Date: ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}
-// :${date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes()}:${date.getSeconds()<10?'0'+date.getSeconds():date.getSeconds()}`
-// console.log(currentDate);
-// console.log(typeof date.getSeconds())
-// output.textContent=currentDate
-
-
-const form = document.querySelector('form')
-
-let todos = []
-
-const createTask = () => {
-    const message = document.querySelector('input')
-    const task = {
-        id: new Date().toISOString(),
-        message: message.value,
-        status: false,
-        date: new Date()
-    }
-    todos = [task,...todos]
-    renderTodos()
-}
-const message = document.querySelector('input')
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    if (message.value === '') {
-        alert('nedostato4no')
+function getStorage() {
+    let todos;
+    let webtasks = localStorage.getItem('localtasks');
+    if (webtasks == null) {
+        todos = [];
     } else {
-        createTask()
-        console.log(todos);
+        todos = JSON.parse(webtasks);
     }
+    return todos;
+}
+
+function setStorage(data) {
+    localStorage.setItem('localtasks', JSON.stringify(data))
+}
+
+addTaskBtn.addEventListener('click', () => {
+    addToStorage()
 })
 
-const renderTodos = () => {
-    const output = document.querySelector('#output')
-    const input = document.querySelector('input')
-    output.innerHTML = ''
-    input.value = ''
-    todos.forEach((el,index) => {
-        const block = document.createElement('div')
-        const buttons = document.createElement('div')
-        const message = document.createElement('p')
-        const statusMessage = document.createElement('p')
-        const dateDom = document.createElement('p')
+function addToStorage() {
+    let addTaskInputVal = taskInputValue.value;
+    if (addTaskInputVal.trim() != 0) {
+        let todos = getStorage();
+        todos.push(addTaskInputVal);
+        setStorage(todos)
+        taskInputValue.value = ''
+        renderTodos()
+    } else {
+        let snackBar = document.getElementById('snackBar');
+        snackBar.className = 'show';
+        setTimeout(function () {
+            snackBar.className = snackBar.className.replace('show', '');
+        }, 3000)
+    }
 
-        const deleteTodo = document.createElement('button')
-        const imageDelete = document.createElement('img')
-        const imageDone = document.createElement('img')
-        const imageEdit = document.createElement('img')
-        const doneTodo = document.createElement('button')
-        const editTodo = document.createElement('button')
-
-        deleteTodo.textContent = 'DELETE'
-        block.className = 'task'
-        buttons.className = 'buttons'
-        doneTodo.textContent = el.status
-            ? 'NOT DONE'
-            : 'DONE'
-        editTodo.textContent = 'EDIT'
-
-        message.textContent = `${index+1}.${el.message}`
-        message.className = 'msgName'
-        message.style.textDecoration = el.status===true
-            ?'line-through'
-            :''
-        imageDelete.src = 'https://w7.pngwing.com/pngs/241/639/png-transparent-bin-icon-computer-icons-desktop-button-free-high-quality-remove-icon-miscellaneous-text-rectangle.png'
-        imageDone.src = 'https://www.clipartmax.com/png/middle/301-3011314_pe-success-icon-task-done.png'
-        imageEdit.src = 'https://toppng.com/uploads/preview/75476-2019-02-08-edit-icon-png-small-11563142463qiwrzqx0e1.png'
-        imageDelete.className = 'imageDlt'
-        imageDone.className = 'imageDn'
-        imageEdit.className = 'imageEdt'
-        statusMessage.textContent = el.status
-            ? 'TODO IS DONE'
-            : 'YOUR TODO IS NOT DONE'
-
-        block.style.background = el.status
-            // ? 'lightgreen'
-            // : 'red'
-
-        const date = el.date
-
-        const currentDate = `Date: ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}`
-
-
-        dateDom.textContent = currentDate
-
-
-        imageDone.addEventListener('click', () => {
-            doneFunc(el.id)
-        })
-
-        imageDelete.addEventListener('click', () => {
-            if(el.status ===true){
-                deleteFunc(el.id)
-            }else{
-                alert('not a done')
-            }
-            
-        })
-
-        imageEdit.addEventListener('click', () => {
-           
-            if(el.status===false){
-                editFunc(el.id)
-            }else{
-                alert('its done')
-            }
-            
-        })
-
-        buttons.append(imageDelete, imageDone, imageEdit)
-        block.append(message, statusMessage, dateDom, buttons)
-        output.append(block)
-    })
-}
-const editFunc = (id) => {
-    todos = todos.map(el => {
-        if (id === el.id) {
-            el.message = prompt('redaktiryute')
-        }
-        return el
-    })
-    renderTodos()
-    console.log(todos)
 }
 
-const doneFunc = (id) => {
-    todos = todos.map(el => {
-        if (id === el.id) {
-            el.status = !el.status
-        }
-        return el
+function renderTodos() {
+    let addedTasksList = document.getElementById('tasks')
+    let todos = getStorage();
+    let html = '';
+    todos.forEach((el, index) => {
+        html += `<div id="task">
+            <span id="taskName">
+                ${index + 1}. ${el}
+            </span>
+            <div id="actions">
+                <button id="delete" onclick="deleteTasks(${index})"><ion-icon name="trash"></ion-icon>Delete</button>
+                <button id="done" onlick="doneTasks(${index})"><ion-icon name="checkmark-done"></ion-icon>Done</button>
+                <button id="edit" onclick="editTasks(${index})"><ion-icon name="create"></ion-icon>Edit</button>
+            </div>
+        </div>`
     })
-    renderTodos()
-    console.log(todos)
+    if (todos.length != 0) {
+        addedTasksList.innerHTML = html;
+    } else {
+        addedTasksList.innerHTML = `<span id="noTasks">There are not tasks to show...</span>`;
+    }
+
 }
 
-const deleteFunc = (id) => {
-    todos = todos.filter(el => {
-        return id !== el.id
-    })
-    renderTodos()
-    console.log(todos)
+function editTasks(index) {
+    let todos = getStorage();
+    taskInputValue.value = todos[index];
+    let saveIndex = document.getElementById('saveIndex');
+    saveIndex.value = index;
+    addTaskBtn.style.display = 'none';
+    saveTaskBtn.style.display = 'block';
 }
 
-const DeleteAll = document.querySelector('.dltAll')
-
-DeleteAll.addEventListener('click',()=>{
-    deleteAllFunc()
+saveTaskBtn.addEventListener('click', () => {
+    saveTasks()
 })
+function saveTasks() {
+    let todos = getStorage()
+    saveIndex = document.getElementById('saveIndex').value;
+    todos[saveIndex] = taskInputValue.value;
+    setStorage(todos)
+    renderTodos()
+    taskInputValue.value = '';
+    addTaskBtn.style.display = 'block';
+    saveTaskBtn.style.display = 'none';
+}
 
-const deleteAllFunc = () =>{
-    if(todos != null){
+function deleteTasks(index) {
+    let todos = getStorage();
+    todos.splice(index, 1);
+    setStorage(todos);
+    renderTodos()
+}
+// function doneTasks(index) {
+//     let todos = getStorage();
+//     taskInputValue.value = todos[index];
+//     taskInputValue.value.style.textDecoration = 'line-through'
+//     setStorage(todos);
+//     renderTodos()
+// }
+
+deleteAll.addEventListener('click', () => {
+    deleteAllTasks()
+})
+function deleteAllTasks() {
+    let todos = getStorage()
+    if (todos != null) {
         todos = []
     }
+    setStorage(todos)
     renderTodos()
+    taskInputValue.value = '';
+    addTaskBtn.style.display = 'block';
+    saveTaskBtn.style.display = 'none';
 }
 
-// 1)Отрисовать дату по человечески
-// 2)После добавления новой тудушки input очищается
-// 3)input не должен принимать пустоту
-// 4)Реализовать удаление тудушки
-// 5)Удаление тудушки после Done(status:true)
-// 6)Реализовать редактирование тудушки
-// 7)редактирование не работает после Done(status:true)
-// 8)Вместо кнопок поставить картинки-иконки
-// 9)После Done(status:true) текст должен перечеркнутся
-// 10)Созданная вами тудушка должна появится первой в списке
-// 11)Пронумеровать ваши тудушки
-// 12)Стилизовать
-// 13)Залить в гитхаб
+searchInput.addEventListener('input',searchTasks)
+
+function searchTasks() {
+    InputValue = searchInput.value;
+    inputValue = inputValue.replace(/^./, str => str.toUpperCase());
+    let tasks = document.querySelectorAll('#task');
+    Array.from(tasks).forEach(function (el) {
+        let taskTxt = el.getElementsByTagName('span')[0].innerText;
+        if (taskTxt.includes(inputValue)) {
+            el.style.display = 'block'
+            el.style.display = 'flex'
+        } else {
+            el.style.display = 'none'
+        }
+    })
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const createTask = () => {
+//     const message = document.querySelector('input')
+//     const task = {
+//         id: new Date().toISOString(),
+//         message: message.value,
+//         status: false,
+//         date: new Date()
+//     }
+//     let todos = getStorage()
+//     todos = [task, ...todos]
+//     setStorage(todos)
+//     renderTodos()
+// }
+
+// const message = document.querySelector('input')
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     if (message.value === '') {
+//         alert('nedostato4no')
+//     } else {
+//         createTask()
+//         console.log(todos);
+//     }
+// })
+
+// const renderTodos = () => {
+//     const output = document.querySelector('#output')
+//     const input = document.querySelector('input')
+//     let todos = getStorage();
+//     let html = '';
+//     output.innerHTML = html;
+//     input.value = ''
+//     todos.forEach((el, index) => {
+//         const block = document.createElement('div')
+//         const buttons = document.createElement('div')
+//         const message = document.createElement('p')
+//         const statusMessage = document.createElement('p')
+//         const dateDom = document.createElement('p')
+
+//         const deleteTodo = document.createElement('button')
+//         const imageDelete = document.createElement('img')
+//         const imageDone = document.createElement('img')
+//         const imageEdit = document.createElement('img')
+//         const doneTodo = document.createElement('button')
+//         const editTodo = document.createElement('button')
+
+//         deleteTodo.textContent = 'DELETE'
+//         block.className = 'task'
+//         buttons.className = 'buttons'
+//         doneTodo.textContent = el.status
+//             ? 'NOT DONE'
+//             : 'DONE'
+//         editTodo.textContent = 'EDIT'
+
+//         message.textContent = `${index + 1}.${el.message}`
+//         message.className = 'msgName'
+//         message.style.textDecoration = el.status === true
+//             ? 'line-through'
+//             : ''
+//         imageDelete.src = '/images/trash.svg'
+//         imageDone.src = '/images/checkmark-done.svg'
+//         imageEdit.src = '/images/create.svg'
+//         imageDelete.className = 'imageDlt'
+//         imageDone.className = 'imageDn'
+//         imageEdit.className = 'imageEdt'
+//         statusMessage.textContent = el.status
+//             ? 'TODO IS DONE'
+//             : 'YOUR TODO IS NOT DONE'
+
+//         block.style.background = el.status
+//         // ? 'lightgreen'
+//         // : 'red'
+
+//         const date = el.date
+
+//         // const currentDate = `Date: ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}`
+
+
+//         // dateDom.textContent = currentDate
+
+
+//         imageDone.addEventListener('click', () => {
+//             doneFunc(el.id)
+//         })
+
+//         imageDelete.addEventListener('click', () => {
+//             if (el.status === true) {
+//                 deleteFunc(el.id)
+//             } else {
+//                 alert('not a done')
+//             }
+
+//         })
+
+//         imageEdit.addEventListener('click', () => {
+
+//             if (el.status === false) {
+//                 editFunc(el.id)
+//             } else {
+//                 alert('its done')
+//             }
+
+//         })
+
+//         buttons.append(imageDelete, imageDone, imageEdit)
+//         block.append(message, statusMessage, dateDom, buttons)
+//         output.append(block)
+//     })
+// }
+// const editFunc = (id) => {
+//     todos = todos.map(el => {
+//         if (id === el.id) {
+//             el.message = prompt('redaktiryute')
+//         }
+//         return el
+//     })
+//     renderTodos()
+//     console.log(todos)
+// }
+
+// const doneFunc = (id) => {
+//     todos = todos.map(el => {
+//         if (id === el.id) {
+//             el.status = !el.status
+//         }
+//         return el
+//     })
+//     renderTodos()
+//     console.log(todos)
+// }
+
+// const deleteFunc = (id) => {
+//     todos = todos.filter(el => {
+//         return id !== el.id
+//     })
+//     renderTodos()
+//     console.log(todos)
+// }
+
+// const DeleteAll = document.querySelector('.dltAll')
+
+// DeleteAll.addEventListener('click', () => {
+//     deleteAllFunc()
+// })
+
+// const deleteAllFunc = () => {
+//     if (todos != null) {
+//         todos = []
+//     }
+//     renderTodos()
+// }
